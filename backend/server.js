90cb1b8f0434c01faecf911bdbb4698b702bd2da
masarkerdev <mahfujsau911@gmail.com>
2026-07-04 10:32:49 +0600

    Fix server listen in production

--- backend/server.js
+++ backend/server.js
@@ -77,9 +77,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, error: err.message });
 });
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
